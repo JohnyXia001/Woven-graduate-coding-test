a440ee0c1702bfd1bde59e429d11bfee2dbec325
JohnyXia001 <45323098+JohnyXia001@users.noreply.github.com>
2021-02-19 12:57:13 +1100

    process all command

--- robot.py
+++ robot.py
@@ -2,11 +2,39 @@ class Solution:
     def __init__(self):
         self.x = 0
         self.y = 0
-        self.direction = None
+        self.direction = 0 # degree from 0-360
 
-    def moving(self,cmd):
+    def process(self, cmd):
+        cmd = cmd.split(",")
+        for pair in cmd:
+            action = pair[0]
+            number = int(pair[1:])
+            print(action, number)
+            self.command(action, number)
 
-        return
+    def command(self, action, number):
+        # turn direction
+        if action == "R":
+            self.direction = (self.direction-90*number)%360
+            return
+        elif action == "L":
+            self.direction = (self.direction+90*number)%360
+            return
+
+        # move
+        if action == "F":
+            cur_dir = self.direction
+        elif action == "B":
+            cur_dir = (self.direction+180)%360
+
+        if cur_dir == 0:
+            self.x += number
+        elif cur_dir == 90:
+            self.y += number
+        elif cur_dir == 180:
+            self.x -= number
+        elif cur_dir == 270:
+            self.y -= number
 
     def distance(self):
         return abs(self.x)+abs(self.y)
@@ -14,6 +42,6 @@ class Solution:
 
 cmd = "F1,R1,B2,L1,B3"
 robot = Solution()
-robot.moving(cmd)
+robot.process(cmd)
 d = robot.distance()
 print(d)
